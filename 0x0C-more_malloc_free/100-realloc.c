1641fa913d064af678c83a1015d7983b8d97a774
bernandez <alouzehm@gmail.com>
2021-08-24 11:33:41 +0100

    added file

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,51 +1,50 @@
 #include <stdlib.h>
 #include "holberton.h"
+
 /**
-* *_realloc -  reallocates a memory block using malloc and free
-* @ptr: void pointer
-* @old_size: already allocated size
-* @new_size: new size to allocate
-* Return: pointer to newly allocated memory or null
-**/
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
+ *
+ * Return: pointer to the newly allocated memory block
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-unsigned char *np;
-unsigned int i;
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
 
-if (new_size == old_size)
-return (ptr);
-if (new_size == 0 && ptr != NULL)
-{
-free(ptr);
-return (NULL)
-}
-if (ptr == NULL)
-{
-ptr = malloc(new_size * sizeof(void *));
-if (ptr == NULL)
-return (NULL);
-return (ptr);
-}
-np = malloc(new_size * sizeof(char));
-if (np == NULL)
-return (NULL);
-i = 0;
-if (new_size > old_size)
-{
-while (i < old_size)
-{
-np[i] = ((char *)ptr)[i];
-i++;
-}
-free(ptr);
-return (np);
-}
-/* if new_size < old_size */
-while (i < new_size)
-{
-np[i] = ((char *)ptr)[i];
-i++;
-}
-free(ptr);
-return (np);
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	free(ptr);
+	return (ptr1);
 }
